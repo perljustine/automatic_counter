88130fddc8ec4c3973c00bd2e2c8779a21f57964
perljustine <justine.perl@hotmail.com>
2025-01-21 12:10:09 +0100

    fin css

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,15 +1,16 @@
 let valueDisplays = document.querySelectorAll(".num");
-let interval = 4000;
+let interval = 40000; // maximum = 40000
+
 
 valueDisplays.forEach((valueDisplay) => {
-  let startValue = 0;
-  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-  let duration = Math.floor(interval / endValue);
+  let startValue = 0; // commencer à zéro
+  let endValue = parseInt(valueDisplay.getAttribute("data-val")); // on vérifie la valeur du data
+  let duration = Math.floor(interval / endValue); // la durée sera l'équivalent entre l'interval etlavaleur de fin Endvalue
   let counter = setInterval(function () {
-    startValue += 1;
-    valueDisplay.textContent = startValue;
+    startValue += 1; // on rajoute 1 a chqaue tour de boucle
+    valueDisplay.textContent = startValue; // le tete est la valeur de chaque nombre
     if (startValue == endValue) {
       clearInterval(counter);
-    }
-  }, duration);
+    } // si les valeur sont egales alors on reset le counter
+  }, duration); // sinon la fonction duration est toujours actuelle 
 });
